9cd26d0914dcd50e6484d761afa4080987720301
Sam Howes <sam@samhowes.com>
2024-08-10 17:05:51 -0400

    VelocityScatter boilerplate

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {VelocityScatterComponent} from "./velocity-scatter/velocity-scatter.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, VelocityScatterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ web/src/app/velocity-scatter/velocity-scatter.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'velocity-scatter',
+  standalone: true,
+  imports: [],
+  templateUrl: './velocity-scatter.component.html',
+  styleUrl: './velocity-scatter.component.scss'
+})
+export class VelocityScatterComponent {
+
+}
