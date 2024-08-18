847d45ce99c6fd2760016e3090c94b899c9c7d4f
Sam Howes <sam@samhowes.com>
2024-08-18 11:39:30 -0400

    Added `WorkState` to not interfere with original state

--- dotnet/Forecasting/WorkItem.cs
+++ dotnet/Forecasting/WorkItem.cs
@@ -1,15 +1,22 @@
-using System.Text.Json.Serialization;
+using SamHowes.Analytics.Forecasting.Teams;
 
 namespace SamHowes.Analytics.Forecasting;
 
-public record AreaRef(string AreaPath);
-
-public enum StateCategory
+public enum WorkState
 {
-    Proposed,
-    InProgress,
+    New,
+    Active,
     Completed
 }
+
+public enum WorkItemState
+{
+    New,
+    Active,
+    Completed
+}
+
+
 
 public record AzureUser(string? UserEmail);
 
@@ -19,7 +26,8 @@ public class WorkItem
     public string? ParentId { get; set; }
     public WorkItem? Parent { get; set; }
     public string Title { get; set; }
-    public string State { get; set; }
+    public WorkItemState State { get; set; }
+    public WorkState WorkState { get; set; }
     public double? StoryPoints { get; set; }
     public double? PointsRemaining { get; set; }
 
@@ -29,9 +37,12 @@ public class WorkItem
     public List<WorkItem> Children { get; set; } = [];
     public int Priority { get; set; } = int.MaxValue;
     
-    public DateTimeOffset StartedAt { get; set; }
+    public DateTimeOffset? StartedAt { get; set; }
     
-    public DateTimeOffset CompletedAt { get; set; }
+    public DateTimeOffset? CompletedAt { get; set; }
     public int Depth { get; set; }
     public bool Workable { get; set; }
+    public Contributor? Contributor { get; set; }
+    public double? DaysRemaining { get; set; }
+    public int Index { get; set; }
 }
